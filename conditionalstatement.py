db6d5ecaffc810e1680979c7e10dc811d5107cb5
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-08-05 19:07:21 +0530

    Add files via upload

---
+++ conditionalstatement.py
@@ -0,0 +1,14 @@
+# example 1
+print("Welcome to the eligible check for voting!")
+height = int(input("What is your age "))
+if height >=18:
+    print("you can vote")
+else:
+    print("you can not vote")
+# example 2
+print("To check the number is even or odd?")
+num = int(input("enter the number:"))
+if num%2 ==0:
+    print("even number")
+else:
+    print("odd number")
