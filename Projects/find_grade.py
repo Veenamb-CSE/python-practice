887d40b62b6175153ce5fd5595c2e3615b7ebd82
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-08-10 20:17:42 +0530

    Add files via upload

---
+++ Projects/find_grade.py
@@ -0,0 +1,22 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades ={}
+for key in student_scores:
+    if student_scores[key]>=91 and student_scores[key]<=100:
+        student_grades[student_scores[key]]="Outstanding"
+    elif student_scores[key]>=81 and student_scores[key]<=90:
+        student_grades[student_scores[key]]="Exceeds Expectations"
+    elif student_scores[key]>=71 and student_scores[key]<=80:
+        student_grades[student_scores[key]]="Acceptable"
+    else:
+        student_grades[student_scores[key]]= "Fail" 
+print(student_grades)
+        
+        
+        
