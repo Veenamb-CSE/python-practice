9c2d8a878d22b163d4cc8ed77c64a9c06f752eae
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-08-14 18:06:55 +0530

    Create maximum_produnt_of_two_elements_in_an_array.py

---
+++ LeetCode/maximum_produnt_of_two_elements_in_an_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max=0
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                product=(nums[i]-1)*(nums[j]-1)
+                if product>max:
+                        max=product
+        return max
+        
