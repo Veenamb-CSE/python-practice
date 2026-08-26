01e8436d1a9704e613eef2be64eec0585399acf9
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-08-26 21:26:38 +0530

    Create Richest_Customer_Wealth.py

---
+++ LeetCode/Richest_Customer_Wealth.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        sumation=[]
+        for i in range(0,len(accounts)):
+            sum(accounts[i])
+            sumation.append(sum(accounts[i]))
+        return max(sumation)
+        
