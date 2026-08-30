1f147d5966e3bc18c6b0173ff842b22af48ebdef
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-08-30 19:28:43 +0530

    Create find_the_highest_altitude.py

---
+++ LeetCode/find_the_highest_altitude.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        altitude = 0
+        highest = 0
+
+        for i in gain:
+            altitude = altitude + i
+
+            if altitude > highest:
+                highest = altitude
+
+        return highest
+               
+
+        
+        
