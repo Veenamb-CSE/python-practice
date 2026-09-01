f3455483d587f4f5a20a64ccf8d2df7811530753
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-09-01 09:48:12 +0530

    Create find_3_digit_even_number.py

---
+++ LeetCode/find_3_digit_even_number.py
@@ -0,0 +1,12 @@
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        list = []
+        for i in range(len(digits)):
+            for j in range(len(digits)): 
+                for k in range(len(digits)): 
+                    if i != j and i != k and j != k: 
+                        if digits[i] != 0 and digits[k] % 2 == 0:
+                             value = digits[i] * 100 + digits[j] * 10 + digits[k] 
+                             if value not in list:
+                                list.append(value) 
+        list.sort() 
+        return list
