685da4bf1d32cd462a4df289e5eb66e86987a240
Veena Mallikarjun <veenabudapanahalli@gmail.com>
2026-09-25 15:41:23 +0530

    Create Larger_substring_between_2_equal_charecter.py

---
+++ LeetCode/Larger_substring_between_2_equal_charecter.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+
+        max_length = -1
+
+        for i in range(len(s)):
+            for j in range(i + 1, len(s)):
+
+                if s[i] == s[j]:
+                    length = j - i - 1
+                    max_length = max(max_length, length)
+
+        return max_length
